167029351637031ef565f57a1b6fbe609a1b7660
Stephen <cheunste@gmail.com>
2020-01-13 14:35:51 -0800

    Refactored and added some unit tests

--- module/ConfigModule.py
+++ module/ConfigModule.py
@@ -1,9 +1,9 @@
 import csv
 
-UCC_PROJECT_CONFIG_FILE='config/UccProjectConfig.csv'
-PDX_HMI_PROJECT_CONFIG_FILE='config/PdxHmiProjectConfig.csv'
-SITE_HMI_PROJECT_CONFIG_FILE='config/SiteHmiProjectConfig.csv'
-FE_PROJECT_CONFIG_FILE='config/FeProjectConfig.csv'
+UCC_PROJECT_CONFIG_FILE = 'config/UccProjectConfig.csv'
+PDX_HMI_PROJECT_CONFIG_FILE = 'config/PdxHmiProjectConfig.csv'
+SITE_HMI_PROJECT_CONFIG_FILE = 'config/SiteHmiProjectConfig.csv'
+FE_PROJECT_CONFIG_FILE = 'config/FeProjectConfig.csv'
 _REQUIRED_FIELD_LENGTH = 3
 
 
@@ -11,7 +11,7 @@ def read_project_sites(config_file_name) -> list:
 	with open(config_file_name) as config_file:
 		config_file.__next__()
 		site_project_csv = csv.reader(config_file)
-		project_list=[]
+		project_list = []
 		for row in site_project_csv:
 			if _is_number_of_fields_valid(row) and _is_field_name_valid(row):
 				project_list.append(row)
--- tests/ConfigurationTests.py
+++ tests/ConfigurationTests.py
@@ -10,3 +10,18 @@ class ConfigurationTest(unittest.TestCase):
 		self.assertTrue(os.path.isfile(Config.UCC_PROJECT_CONFIG_FILE))
 		ucc_config_list = Config.read_project_sites(Config.UCC_PROJECT_CONFIG_FILE)
 		self.assertTrue(len(ucc_config_list) == 2)
+
+	def test_read_site_hmi_configuration_file(self):
+		self.assertTrue(os.path.isfile(Config.SITE_HMI_PROJECT_CONFIG_FILE))
+		ucc_config_list = Config.read_project_sites(Config.SITE_HMI_PROJECT_CONFIG_FILE)
+		self.assertTrue(len(ucc_config_list) == 2)
+
+	def test_read_fe_configuration_file(self):
+		self.assertTrue(os.path.isfile(Config.FE_PROJECT_CONFIG_FILE))
+		ucc_config_list = Config.read_project_sites(Config.FE_PROJECT_CONFIG_FILE)
+		self.assertTrue(len(ucc_config_list) == 2)
+
+	def test_read_pdx_hmi_configuration_file(self):
+		self.assertTrue(os.path.isfile(Config.PDX_HMI_PROJECT_CONFIG_FILE))
+		ucc_config_list = Config.read_project_sites(Config.PDX_HMI_PROJECT_CONFIG_FILE)
+		self.assertTrue(len(ucc_config_list) == 2)
